27c799719a50007dfb8279abd4e4efe4efcafdfc
t1i0my9a <miuratatuya1109@gmail.com>
2025-03-18 17:46:05 +0900

    Add files via upload

---
+++ predict stock trends.py
@@ -0,0 +1,15 @@
+import yfinance as yf #get imformation from yahoo finance
+import pandas as pd #data frame proces
+
+#get information about index
+index = {
+    "S&P500": "^GSPC",#ticker symbol
+    "Nikkei 225": "^N225",
+    "EEM (Emerging Markets)": "EEM",
+    "Euro Stoxx 50": "^STOXX50E"
+}
+
+#setting start date and finish date
+start_date = "2000-01-01"
+finish_date = "2025-01-01"
+
